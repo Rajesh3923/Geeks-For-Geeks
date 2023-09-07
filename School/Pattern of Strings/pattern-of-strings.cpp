46d050b99da31ac91a4378eeaed41aecaca8c07f
Rajesh pasupuleti <115516128+Rajesh3923@users.noreply.github.com>
2023-09-07 23:58:59 +0530

    Added Solution - GfG to GitHub

---
+++ School/Pattern of Strings/pattern-of-strings.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	vector<string> pattern(string S)
+	{
+	    vector<string> v;
+	    
+	    for(int i=S.length()-1;i>=0;i--)
+	    {
+	        v.push_back(S);
+	        S.pop_back();
+	    }
+	    
+	    
+	    return v;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   		Solution ob;
+
+   		vector<string> ans = ob.pattern(s);
+
+   		for(int i = 0; i < ans.size(); i++)
+   			cout << ans[i] << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
