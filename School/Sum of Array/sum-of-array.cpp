a88a2622d74b4eaa1dc0f0b79ff87567e269830e
Rajesh pasupuleti <115516128+Rajesh3923@users.noreply.github.com>
2023-09-14 22:13:35 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of Array/sum-of-array.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int c=0;
+	int sum(int arr[], int n) {
+	   for (int i=0;i<n;i++){
+	       c+=arr[i];
+	   }return c;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.sum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
