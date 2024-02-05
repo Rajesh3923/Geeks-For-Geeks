65186d063547fb817d36d9b1d9deaae3f443b081
Rajesh Pasupuleti <115516128+Rajesh3923@users.noreply.github.com>
2024-02-05 23:28:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Assign Cookies/assign-cookies.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      int a[] = new int[n];
+      for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+      }
+      int b[] = new int[m];
+      for(int i=0;i<m;i++){
+        b[i]=sc.nextInt();
+      }
+      
+      Solution obj = new Solution();
+      int ans = obj.maxChildren(n, m,a, b);
+      System.out.println(ans);
+    }
+  }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int maxChildren(int N,int M,int greed[], int sz[]) {
+        int i=0;int j=0;int ans=0;
+        Arrays.sort(greed);
+        Arrays.sort(sz);
+        while(i<M && j<N){
+            if(sz[i]>=greed[j]){
+                ans++;
+                j++;
+                i++;
+                
+            }
+            else{
+                i++;
+                
+                
+            }
+        }return ans;
+        
+        
+    
+
+  }
+}
+     
