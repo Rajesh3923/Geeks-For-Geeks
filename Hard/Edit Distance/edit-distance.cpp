0b74a0bd6a4692878fcf7610c97b8223e4960fbd
Rajesh Pasupuleti <115516128+Rajesh3923@users.noreply.github.com>
2024-04-19 19:46:17 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Edit Distance/edit-distance.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+   int editDistance(string s, string t) {
+       const int m = s.length();
+    const int n = t.length();
+    
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+    for (int i = 1; i <= m; ++i)
+      dp[i][0] = i;
+
+    for (int j = 1; j <= n; ++j)
+      dp[0][j] = j;
+
+    for (int i = 1; i <= m; ++i)
+      for (int j = 1; j <= n; ++j)
+        if (s[i - 1] == t[j - 1])
+          dp[i][j] = dp[i - 1][j - 1];
+        else
+          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+
+    return dp[m][n]; // Code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
