d6a5548d26e55f5360aef3e6e01163bb339f1e8e
Rajesh Pasupuleti <115516128+Rajesh3923@users.noreply.github.com>
2024-06-11 21:37:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse First K elements of Queue/reverse-first-k-elements-of-queue.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+class ModifyQueue {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // Taking count of total number of elements
+            int n = sc.nextInt();
+
+            // Taking count of total elements
+            // that need to be reversed
+            int k = sc.nextInt();
+
+            // Creating a Queue
+            Queue<Integer> q = new LinkedList<>();
+
+            // adding all the elements to the Queue
+            while (n-- > 0) {
+                q.add((int)sc.nextInt());
+            }
+
+            // Creating an object of class GfG
+            GfG g = new GfG();
+
+            // calling modifyQueue of class GfG
+            // and passing Queue and k as arguments
+            // and storing the reuslt in a new Queue
+            Queue<Integer> ans = g.modifyQueue(q, k);
+
+            // Printing all the elements from the
+            // new Queue and polling them out
+            while (!ans.isEmpty()) {
+                int a = ans.peek();
+                ans.poll();
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class GfG {
+    // Function to reverse first k elements of a queue.
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+        Stack<Integer> stack = new Stack<>();
+        
+        // Step 1: Push the first k elements into a stack
+        for (int i = 0; i < k; i++) {
+            stack.push(q.poll());
+        }
+        
+        // Step 2: Enqueue the elements from the stack back into the queue
+        while (!stack.isEmpty()) {
+            q.add(stack.pop());
+        }
+        
+        // Step 3: Remove the remaining elements and enqueue them back to preserve order
+        int remainingSize = q.size() - k;
+        for (int i = 0; i < remainingSize; i++) {
+            q.add(q.poll());
+        }
+        
+        return q;
+    }
+}
