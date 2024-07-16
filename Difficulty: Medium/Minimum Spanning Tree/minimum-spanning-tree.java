1e325c1a5d7ecfb3ff767a2434a22b6c21aadcd7
Rajesh Pasupuleti <115516128+Rajesh3923@users.noreply.github.com>
2024-07-16 19:02:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -35,15 +35,99 @@ public class Main {
 // } Driver Code Ends
 
 
-// User function Template for Java
+class Solution {
+
+    static int spanningTree(int V, int E, List<List<int[]>> adj) {
+        int[] size = new int[V];
+        int[] parent = new int[V];
+        
+        for(int i = 0; i < V; i++) {
+            size[i] = 1;
+            parent[i] = i;
+        }
+        
+        List<Edge> edges = new ArrayList<Edge>();
+        
+        for(int i = 0; i < V; i++) {
+            int ajdNodeListSize = adj.get(i).size();
+            for(int j = 0; j < ajdNodeListSize; j++) {
+                int srcNode = i;
+                int destNode = adj.get(i).get(j)[0];
+                int weight = adj.get(i).get(j)[1];
+                
+                edges.add(new Edge(srcNode, destNode, weight));
+            }
+        }
+        
+        Collections.sort(edges);
+        int mst = 0;
+        int edgeSize = edges.size();
+        
+        for(int i = 0; i < edgeSize; i++) {
+            int node = edges.get(i).src;
+            int destination = edges.get(i).dest;
+            int wt = edges.get(i).weight;
+            
+            if(findParent(parent, node) != findParent(parent, destination)) {
+                mst += wt;
+                unionBySize(parent, size, node, destination);
+            }
+        }
+        
+        return mst;
+        
+    }
+    
+    static int findParent(int[] parent, int node) {
+        if(parent[node] == node) {
+            return node;
+        }
+        
+        parent[node] = findParent(parent, parent[node]);
+        return parent[node];
+    }
+    
+    static void unionBySize(int[] parent, int[] size, int u, int v) {
+        int parentU = findParent(parent, u);
+        int parentV = findParent(parent, v);
+        
+        if(parentU == parentV) {
+            return; // belongs to same component or parent
+        } else if(size[parentU] < size[parentV]) {
+            parent[parentU] = parentV;
+            size[parentV] += size[parentU];
+        } else  {
+            parent[parentV] = parentU;
+            size[parentU] += size[parentV];
+        }
+    }
+}
+
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+    Edge(int src, int dest, int wt) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = wt;
+    }
+    
+    public int compareTo(Edge compareEdge) {
+        return this.weight - compareEdge.weight;
+    }
+}
+
+// using the Prims Algo
+
+
+/*
 class Pair{
     int w;
     int node;
-    int parent;
-    public Pair(int w,int node,int parent){
+    // int parent;
+    public Pair(int w,int node){
         this.w=w;
         this.node=node;
-        this.parent=parent;
+        // this.parent=parent;
     }
 }
 class Solution {
@@ -51,23 +135,22 @@ class Solution {
        PriorityQueue<Pair>pq=new PriorityQueue<>((x,y)->x.w-y.w);
        int []v=new int[V];
        Arrays.fill(v,0);
-       pq.add(new Pair(0,0,-1));
+       pq.add(new Pair(0,0));
        int sum=0;
        while(!pq.isEmpty()){
            Pair p=pq.poll();
            int w=p.w;
            int node=p.node;
-           int parent=p.parent;
+        //   int parent=p.parent;
            if(v[node]==0){
                v[node]=1;
                sum+=w;
                for(int []arr:adj.get(node)){
-                   pq.add(new Pair(arr[1],arr[0],node));
+                   pq.add(new Pair(arr[1],arr[0]));
                    
                    
                }
            }
            
        }return sum;
-    }
-}
+    }*/
