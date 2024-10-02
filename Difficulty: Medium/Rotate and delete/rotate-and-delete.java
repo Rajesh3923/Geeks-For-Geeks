70929670a5b4aba5410ce4ef6e0f7c9cc072dc50
Rajesh Pasupuleti <pasupuletirajesh460@gmail.com>
2024-10-02 16:07:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate and delete/rotate-and-delete.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+    public static int rotateDelete(ArrayList<Integer> arr) {
+        // code here
+        int k=1;
+        int req=arr.size()/2;
+        while(k<=req && arr.size()>1){
+            arr.add(0,arr.get(arr.size()-1));
+            arr.remove(arr.size()-1);
+            arr.remove(arr.size()-k+1-1);
+            k++;
+            
+        }
+        return arr.get(0);
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> arr = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                arr.add(Integer.parseInt(token));
+            }
+
+            Solution obj = new Solution();
+            int res = obj.rotateDelete(arr);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
